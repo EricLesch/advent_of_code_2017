d9f8d6508b556f4858e4bbd0c66d504b38897dd4
Eric Lesch <eric.r.lesch@gmail.com>
2018-02-09 21:27:39 -0600

    Solving Day 3

---
+++ src/main/scala/Day_3.scala
@@ -0,0 +1,153 @@
+//    --- Day 3: Spiral Memory ---
+//    You come across an experimental new kind of memory stored on an infinite two-dimensional grid.
+//
+//    Each square on the grid is allocated in a spiral pattern starting at a location marked 1 and then counting up while spiraling outward. For example, the first few squares are allocated like this:
+//
+//    17  16  15  14  13
+//    18   5   4   3  12
+//    19   6   1   2  11
+//    20   7   8   9  10
+//    21  22  23---> ...
+//    While this is very space-efficient (no squares are skipped), requested data must be carried back to square 1 (the location of the only access port for this memory system) by programs that can only move up, down, left, or right. They always take the shortest path: the Manhattan Distance between the location of the data and square 1.
+//
+//    For example:
+//
+//        Data from square 1 is carried 0 steps, since it's at the access port.
+//        Data from square 12 is carried 3 steps, such as: down, left, left.
+//        Data from square 23 is carried only 2 steps: up twice.
+//    Data from square 1024 must be carried 31 steps.
+//    How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?
+//
+//    Your puzzle answer was 326.
+//
+//    --- Part Two ---
+//        As a stress test on the system, the programs here clear the grid and then store the value 1 in square 1. Then, in the same allocation order as shown above, they store the sum of the values in all adjacent squares, including diagonals.
+//
+//    So, the first few squares' values are chosen as follows:
+//
+//        Square 1 starts with the value 1.
+//    Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+//    Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+//    Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+//    Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+//    Once a square is written, its value does not change. Therefore, the first few squares would receive the following values:
+//
+//    147  142  133  122   59
+//    304    5    4    2   57
+//    330   10    1    1   54
+//    351   11   23   25   26
+//    362  747  806--->   ...
+//    What is the first value written that is larger than your puzzle input?
+//
+//    Your puzzle answer was 363010.
+//
+//    Both parts of this puzzle are complete! They provide two gold stars: **
+//
+//    At this point, you should return to your advent calendar and try another puzzle.
+//
+//    Your puzzle input was 361527.
+
+import scala.collection.mutable.ArrayBuffer
+
+object Day_3 {
+    // Exercise 1
+    // This solution ignores the whole node thing and just works off the pattern that I could see in the generated values
+    def getFluctuatingInterval(level: Int, nth: Int) : Int = {
+        if (nth < level) {
+           nth
+        } else {
+            level - (nth % level)
+        }
+    }
+
+    def _getLevel(count: Int): Int = {
+        scala.math.ceil((scala.math.sqrt(count.toDouble) - 1.0) / 2.0).toInt
+    }
+
+    def getSteps(count: Int): Int = {
+        val level = _getLevel(count)
+        val offset = level - 1
+
+        getFluctuatingInterval(level, (count + offset) % (level * 2)) + level
+    }
+
+    // Exercise 2
+    case class Point(x: Int, y: Int)
+
+    case class Node(point: Point, value: Int = 0)
+
+    object Direction extends Enumeration {
+        type Direction = Value
+        val UP, DOWN, LEFT, RIGHT = Value
+    }
+
+    val DirectionOrder = Array(Direction.RIGHT, Direction.UP, Direction.LEFT, Direction.DOWN)
+
+    // Exercise 2
+    def _generateDirectionsList(numberOfSteps: Int): List[Direction.Direction] = {
+        val arrayOfDirections: ArrayBuffer[Direction.Direction] = ArrayBuffer()
+
+        var directionCounter = 0
+        var numOfStepsToTakeInEachDirectionCounter = 1
+
+        import scala.util.control.Breaks._
+        breakable {
+            var i = 0
+            while (true) {
+                for (_ <- 1 to 2) {
+                    var currentDirection = DirectionOrder(directionCounter % DirectionOrder.length)
+                    for (_ <- 1 to numOfStepsToTakeInEachDirectionCounter) {
+                        arrayOfDirections += currentDirection
+                        i += 1
+                        if (i == numberOfSteps) {
+                            break
+                        }
+                    }
+                    directionCounter += 1
+                }
+                numOfStepsToTakeInEachDirectionCounter += 1
+            }
+        }
+
+        arrayOfDirections.toList
+    }
+
+    def _generateNodeList(numberOfSteps: Int): List[Node] = {
+        val stepDirections = _generateDirectionsList(numberOfSteps)
+
+        val nodeList = List(Node(Point(x = 0, y = 0), value = 1))
+
+        addNodes(stepDirections, nodeList).reverse
+    }
+
+    def addNodes(directionsList: List[Direction.Direction], acc: List[Node]): List[Node] = {
+        directionsList.foldLeft(acc)(
+            (acc: List[Node], direction: Direction.Direction) => {
+                val accHeadPoint = acc.head.point
+                val newNodePoint = direction match {
+                    case Direction.RIGHT => Point(accHeadPoint.x + 1, accHeadPoint.y)
+                    case Direction.UP => Point(accHeadPoint.x, accHeadPoint.y + 1)
+                    case Direction.LEFT => Point(accHeadPoint.x - 1, accHeadPoint.y)
+                    case Direction.DOWN => Point(accHeadPoint.x, accHeadPoint.y - 1)
+                }
+
+                val adjacentNodes: List[Node] = acc.filter((node: Node) => _pointsAreWithin1Step(node.point, newNodePoint))
+
+                Node(
+                    point = newNodePoint,
+                    value = adjacentNodes.map(_.value).sum
+                ) :: acc
+            }
+        )
+    }
+
+    def _pointsAreWithin1Step(point1: Point, point2: Point): Boolean = {
+        math.abs(point1.x - point2.x) <= 1 &&
+            math.abs(point1.y - point2.y) <= 1
+    }
+
+    def getNodeWithHigherValue(value: Int): Option[Node] = {
+        // this is a bit lazy and definitely suboptimal - more optimal would be progressively generating nodes until you get the value you need
+        _generateNodeList(200).find((node:Node) => node.value > value)
+    }
+}
---
+++ src/test/scala/Day_3_Tests.scala
@@ -0,0 +1,87 @@
+import org.scalatest.FunSpec
+import Day_3._
+
+class Day_3_Tests extends FunSpec {
+    // Exercise 1
+    describe("_getLevel(count: Int): Int") {
+        it("should return the correct data for the passed values") {
+            assert(_getLevel(2) == 1)
+            assert(_getLevel(8) == 1)
+            assert(_getLevel(9) == 1)
+
+            assert(_getLevel(10) == 2)
+            assert(_getLevel(25) == 2)
+
+            assert(_getLevel(26) == 3)
+            assert(_getLevel(48) == 3)
+            assert(_getLevel(49) == 3)
+
+            assert(_getLevel(50) == 4)
+        }
+    }
+
+    describe("getSteps(count: Int): Int") {
+        it("should return the correct data for the passed steps") {
+            assert(getSteps(2) == 1)
+            assert(getSteps(4) == 1)
+            assert(getSteps(5) == 2)
+            assert(getSteps(9) == 2)
+            assert(getSteps(10) == 3)
+            assert(getSteps(17) == 4)
+
+            assert(getSteps(25) == 4)
+            assert(getSteps(36) == 5)
+
+            assert(getSteps(361527) == 326)
+        }
+    }
+
+    describe("_generateDirectionsList(numberOfSteps: Int): List[Direction.Direction]") {
+        it("should generate a list of step directions for the the given number of steps") {
+            val testList = _generateDirectionsList(20)
+
+            val expectedList = List(Direction.RIGHT, Direction.UP, Direction.LEFT, Direction.LEFT, Direction.DOWN, Direction.DOWN, Direction.RIGHT, Direction.RIGHT, Direction.RIGHT, Direction.UP, Direction.UP, Direction.UP, Direction.LEFT, Direction.LEFT, Direction.LEFT, Direction.LEFT, Direction.DOWN, Direction.DOWN, Direction.DOWN, Direction.DOWN)
+
+            assert(testList == expectedList)
+        }
+    }
+
+    describe("pointsAreWithin1Step(point1: Point, point2: Point): Boolean") {
+        it("should return true if the points are within 1 step of each other") {
+            val testPoint1 = Point(0,0)
+            val testPoint2 = Point(1,1)
+            val testPoint3 = Point(0,1)
+            val testPoint4 = Point(-1,0)
+            val testPoint5 = Point(1, 0)
+
+            assert(_pointsAreWithin1Step(testPoint1, testPoint2))
+            assert(_pointsAreWithin1Step(testPoint2, testPoint3))
+            assert(_pointsAreWithin1Step(testPoint1, testPoint3))
+
+            assert(!_pointsAreWithin1Step(testPoint4, testPoint5))
+            assert(!_pointsAreWithin1Step(testPoint2, testPoint4))
+        }
+    }
+
+    describe("_generateNodeList(numberOfSteps: Int): List[Node]") {
+        it("should return the correct points for each node in the generated nodeList ") {
+            val nodeList = _generateNodeList(20)
+
+            val pointList: Seq[Day_3.Point] = nodeList.map(_.point)
+
+            val expectedPointList = List(Point(0, 0), Point(1, 0), Point(1, 1), Point(0, 1), Point(-1, 1), Point(-1, 0), Point(-1, -1), Point(0, -1), Point(1, -1), Point(2, -1), Point(2, 0), Point(2, 1), Point(2, 2), Point(1, 2), Point(0, 2), Point(-1, 2), Point(-2, 2), Point(-2, 1), Point(-2, 0), Point(-2, -1), Point(-2, -2))
+
+            assert(pointList == expectedPointList)
+
+            val nodeList2 = _generateNodeList(100)
+        }
+    }
+
+    describe("getNodeWithHigherValue(value:Int): : Option[Node]") {
+        it("should return the first node that has a higher value than the passed value") {
+            assert(getNodeWithHigherValue(2).get.value == 4)
+
+            assert(getNodeWithHigherValue(361527).get.value == 363010)
+        }
+    }
+}
